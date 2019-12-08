d97b58a07edaead0a379bf22a2f1f4572c4321ee
Adam Wight <adam.wight@wikimedia.de>
2019-12-08 19:10:41 +0100

    update to varnish 1.0.0 - lose mods

--- manifests/director.pp
+++ manifests/director.pp
@@ -1,19 +1,14 @@
 #director.pp
-define varnish::director ( $type = 'round-robin',
-                           $backends = [],
-                        ) {
+define varnish::director(
+  $type = 'round-robin',
+  $backends = [],
+) {
 
-  validate_re($title,'^[A-Za-z0-9_]*$', "Invalid characters in director name $title. Only letters, numbers and underscore are allowed.")
+  validate_re($title,'^[A-Za-z0-9_]*$', "Invalid characters in director name ${title}. Only letters, numbers and underscore are allowed.")
 
-  concat {
-    "${varnish::vcl::includedir}/directors.vcl":
-      ensure => present, 
-  }
-
-  concat::fragment { "$title-director":
-    target => "${varnish::vcl::includedir}/directors.vcl",
+  concat::fragment { "${title}-director":
+    target  => "${varnish::vcl::includedir}/directors.vcl",
     content => template('varnish/includes/directors.vcl.erb'),
-    order => '02',
+    order   => '02',
   }
-
 }
