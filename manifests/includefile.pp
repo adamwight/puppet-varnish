11f3667d7ff1ea5eba6496704aa9eb48a210b80c
Adam Wight <adam.wight@wikimedia.de>
2020-06-02 22:37:43 +0200

    Move define out to satisfy puppet 5.5.19

---
+++ manifests/includefile.pp
@@ -0,0 +1,16 @@
+define varnish::includefile {
+  $selectors = $varnish::vcl::selectors
+  concat { "${varnish::vcl::includedir}/${title}.vcl":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    notify  => Service['varnish'],
+    require => File[$varnish::vcl::includedir],
+  }
+
+  concat::fragment { "${title}-header":
+    target  => "${varnish::vcl::includedir}/${title}.vcl",
+    content => "# File managed by Puppet\n",
+    order   => '01',
+  }
+}
